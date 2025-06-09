2d4f2c6e210af7733547adabe36a45fb55ef3431
Alex Golubev <golubev-alx@proton.me>
2025-06-09 12:23:22 +0300

    Remove users table schema and update metadata for new schema structure
    - Deleted the `users` table schema from SQL file.
    - Updated journal and snapshot metadata to reflect changes in the schema, including renaming the tag from `0000_black_logan` to `0000_bizarre_logan`.
    - Adjusted user schema in TypeScript to include timezone support for timestamps and added `is_active` field.

--- drizzle/0000_bizarre_logan.sql
+++ drizzle/0000_bizarre_logan.sql
@@ -1,10 +1,11 @@
 CREATE TABLE "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	"name" varchar(255) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"name" varchar(255),
 	"email" varchar(255) NOT NULL,
 	"password" varchar(255) NOT NULL,
 	"avatar_url" text,
+	"is_active" boolean DEFAULT false NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
